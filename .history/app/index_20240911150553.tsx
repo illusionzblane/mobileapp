1442c5a144e16e9b825e6864d6bf56eafb159785
Samantha Yan <yyan30@my.bcit.ca>
2024-09-11 15:09:40 -0700

    adding profile

--- .history/app/index_20240911150553.tsx
+++ .history/app/index_20240911150553.tsx
@@ -12,10 +12,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Profile
-        bioName="Michael Bennett"
-        bioInfo="James Miller is a New York-based digital marketing strategist with a knack for helping brands tell their stories through data-driven insights. When he's not crafting compelling campaigns, you can find him exploring new cities, diving into a good book, or enjoying a strong cup of coffee. Passionate about fitness and food, James believes in balancing work and life with a dash of adventure."
-      />
       <InputForm
               textForName="Michael Bennett"
               textForPlaceholder="Good Job."/>
