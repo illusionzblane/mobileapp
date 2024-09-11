62384900f573757eb7a854c394abe5387cdae653
Brian Paragas <brianparagas49@gmail.com>
2024-09-11 15:29:21 -0700

    updating file name

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 import Name from "@/components/atoms/Name";
-import InputForm from "@/components/molecules/InputForm";
+import Post from "@/components/molecules/Post";
 import Profile from "@/components/molecules/Profile";
 
 export default function Index() {
@@ -16,7 +16,7 @@ export default function Index() {
         bioName="Michael Bennett"
         bioInfo="James Miller is a New York-based digital marketing strategist with a knack for helping brands tell their stories through data-driven insights. When he's not crafting compelling campaigns, you can find him exploring new cities, diving into a good book, or enjoying a strong cup of coffee. Passionate about fitness and food, James believes in balancing work and life with a dash of adventure."
       />
-      <InputForm
+      <Post
               textForName="Michael Bennett"
               textForPlaceholder="Good Job."/>
     </View>
